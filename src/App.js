665b43a7e3a4fc571cdef2ee90e19b16a3427ba5
GrayHead <ss.zhuravlov@gmail.com>
2023-09-11 20:38:38 +0300

    lesson 1-1 basic components + css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from "./components/first-component/FirstComponent";
 
 function App() {
     return (
         <div className="App">
+            <FirstComponent/>
+            <FirstComponent/>
 
         </div>
     );
---
+++ src/components/first-component/FirstComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './FirstComponent.module.css'
+
+const FirstComponent = () => {
+
+    return (
+        <div>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto at beatae dicta dolorem
+                eaque id nihil quibusdam reprehenderit voluptate. Consequatur dicta minima nobis nostrum quis vel velit,
+                veritatis vitae?
+            </p>
+            <img src="https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png" alt=""/>
+
+
+            <hr/>
+        </div>
+    );
+};
+
+export default FirstComponent;
