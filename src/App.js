47e1e4505cb43bf9447a9e692b2f04ea405a2e14
GrayHead <ss.zhuravlov@gmail.com>
2023-09-11 20:50:23 +0300

    lesson 1-2 props

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import FirstComponent from "./components/first-component/FirstComponent";
 function App() {
     return (
         <div className="App">
-            <FirstComponent/>
-            <FirstComponent/>
+            <FirstComponent
+                txt={'homer'}
+                image={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}
+            />
+
+            <FirstComponent
+                txt={'bart'}
+                image={'https://static.wikia.nocookie.net/simpsons/images/b/bd/Homer_Simpson.png'}
+            />
 
         </div>
     );
