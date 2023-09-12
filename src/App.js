aa0c9b1375d1075a440db62ef5aa0bfb810bbfa7
GrayHead <ss.zhuravlov@gmail.com>
2023-09-12 13:06:59 +0300

    lesson  3-4 select+options

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ import {createRef, useState} from "react";
 import {useForm} from "react-hook-form";
 
 function App() {
-    let {register, handleSubmit} = useForm();
+    let {register, handleSubmit, watch, formState: {errors}} = useForm({defaultValues: {title: 'title default'}});
     const onSubmit = (data) => {
         console.log(data);
-
     }
+
+
     return (<div>
 
 
         <form onSubmit={handleSubmit(onSubmit)}>
             <input {...register("title", {required: true})}/>
+            {errors.title && <span>field is required</span>}
             <input {...register("body")}/>
             <input type="submit" value={'save post'}/>
+
+            <select {...register("userId")}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>
         </form>
 
     </div>);
