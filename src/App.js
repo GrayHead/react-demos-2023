144d11293d958b003596740819434c5a504bdd31
GrayHead <ss.zhuravlov@gmail.com>
2023-09-12 10:44:39 +0300

    lesson 1-4 useState useEffect fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+
+    let [counter, setCounter] = useState(0);
+
+    const onClickIncrement = () => {
+        counter++;
+        setCounter(counter);
+    };
+    const onClickDecrement = () => {
+        counter--;
+        setCounter(counter);
+    };
     return (
         <div className="App">
+
+            <h1>counter: {counter} </h1>
+            <button onClick={onClickIncrement}>+</button>
+            <button onClick={onClickDecrement}>-</button>
+
 
         </div>
     );
