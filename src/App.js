ee62a15cf0e711f4d8c47916644fb6e8b1dd7c6d
GrayHead <ss.zhuravlov@gmail.com>
2023-09-12 12:59:46 +0300

    lesson  3-2 forms 2 create ref

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import {createRef, useState} from "react";
 
 function App() {
-    // let [login, setLogin] = useState('default');
-    // let [password, setPassword] = useState('');
-    // const onLoginChange = (e) => {
-    //     setLogin(e.target.value);
-    // };
-    // const onPasswordChange = (e) => {
-    //     setPassword(e.target.value);
-    // };
-    let [formState, setFormState] = useState({login: '', password: ''});
-
+    let login = createRef();
+    let password = createRef();
     const onSubmit = (e) => {
-        console.log(e.target.login.value);
-        console.log(e.target.password.value);
         e.preventDefault();
-        console.log(formState);
-    };
+        console.log(login.current.value)
+        console.log(password.current.value)
+        // create obj & send to api
 
-    const onChange = (e) => {
-        setFormState({...formState, [e.target.name]: e.target.value})
     }
+
     return (<div>
         <form onSubmit={onSubmit}>
-            <input type="text" name={'login'} value={formState.login} onChange={onChange}/>
-            <input type="text" name={'password'} value={formState.password} onChange={onChange}/>
-            <button>submit</button>
+            <input type="text" name={'login'} ref={login}/>
+            <input type="text" name={'password'} ref={password}/>
+            <button>log in</button>
+
         </form>
+
     </div>);
 }
 
