da8b2f17d080e19ee129ca0434dbca9c7860c45d
Vitaliy Dechyshyn <demchyshyn.v87@gmail.com>
2023-10-31 14:58:44 +0200

    context

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import {App} from './App';
@@ -6,7 +5,6 @@ import {App} from './App';
 const root =
     ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
-
+    <App/>
 );
 
